944fae7a3e7909ec0ffe287fabaa0148a7565924
slopez-padilla-byte <slopez-padilla@ucsb.edu>
2026-02-02 18:37:01 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide() {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+  index = (index + 1) % slides.length;
+}
+
+showSlide();
+setInterval(showSlide, 4000); // 4 seconds per slide
